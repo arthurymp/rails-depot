5b83019e2e70001b16a0a21585891e28b93cc7cf
ArthurYMP <moupeng.yang@gmail.com>
2013-03-31 17:08:31 +0800

    Delete README file for clean code

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,23 @@ class StoreController < ApplicationController
   def index
 	@products = Product.find_products_for_sale
   end
+public
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @cart.add_product(product)
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    flash[:notice] = "Invalid product"
+    redirect_to :action => 'index'
+  end
+
+private
+  def find_cart 
+    unless session[:cart]
+      session[:cart] =Cart.new
+    end
+    session[:cart]
+  end
+
 end
